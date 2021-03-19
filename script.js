f3162c5c2c1d7677795ed3c162db0e49f533a89f
Ben Pemberton <benedictpemberton@gmail.com>
2021-03-19 15:58:18 +0000

    Working version. Buttons and gameplay is fine. Some things could be tidied up and script made neater. Confirm buttons disappear too quickly and not sure why that is.

--- script.js
+++ script.js
@@ -9,35 +9,85 @@ window.onload = () => {
     const versusBox = document.getElementById('versusbox');  
     const stickmanBox = document.getElementById('stickmanbox');
     const stickImg = document.createElement('img');
-    stickImg.src = '/images/stickman-icon.png';
+    stickImg.src = 'images/stickman-icon.png';
     const scoreBox = document.getElementById('scorebox');
     const laptopBox = document.getElementById('laptopbox');
     const laptopImg = document.createElement('img');
-    laptopImg.src = '/images/laptop-icon.png';
+    laptopImg.src = 'images/laptop-icon.png';
     const commentBox = document.getElementById('commentbox');
+    const commentText = document.createElement('p');
     const weaponBox = document.getElementById('weaponbox');
     const rockBox = document.getElementById('rockbox');
     const rockImg = document.createElement('img');
     rockImg.setAttribute('id', 'rockimg');
-    rockImg.src = '/images/rock.png';
+    rockImg.src = 'images/rock.png';
     const paperBox = document.getElementById('paperbox');
     const paperImg = document.createElement('img');
     paperImg.setAttribute('id', 'paperimg');
-    paperImg.src = '/images/paper.png';
+    paperImg.src = 'images/paper.png';
     const scissorBox = document.getElementById('scissorbox');
     const scissorImg = document.createElement('img');
     scissorImg.setAttribute('id', 'scissorimg');
-    scissorImg.src = '/images/scissors.png';
+    scissorImg.src = 'images/scissors.png';
+    const weaponIcons = document.getElementById('weaponicons');
     const replayButton = document.createElement('button');
     replayButton.setAttribute('class', 'buttons');
+    replayButton.setAttribute('id', 'replaybtn');
     replayButton.textContent = 'Replay';
     replayButton.style.margin = 'auto';
-    const weaponPrompt = document.getElementById('weaponprompt');
     const letsPlay = document.querySelector('#btnyes');
+    const noPlay = document.querySelector('#btnno');
+
+    noPlay.addEventListener('click', () => {
+        clearIntro();
+    });
 
     letsPlay.addEventListener('click', () => {
         startPlay();
     });
+
+    commentBox.appendChild(commentText);
+    pointsText.innerHTML = '0 - 0';
+    scoreBox.appendChild(pointsText);
+    const weaponsDivs = document.querySelectorAll('.weapons');
+
+    weaponsDivs.forEach(function(node) {
+        node.addEventListener('mouseover', weaponsHover);
+        node.addEventListener('mouseout', weaponsOff);
+    });
+    
+    weaponsDivs.forEach(function(node) {
+        node.addEventListener('click', function(e) {
+            if (e.currentTarget.id === 'rockbox') {
+                playerThisRound = 'Rock';
+            } else if (e.currentTarget.id === 'paperbox') {
+                playerThisRound = 'Paper';
+            } else if (e.currentTarget.id === 'scissorbox') {
+                playerThisRound =  'Scissors';
+            }
+            playRound(playerThisRound, computerPlay());
+            pointsText.innerHTML = `${playerPoints} - ${computerPoints}`;
+            if (roundsPlayed === 5) {
+                results();
+            }
+        });
+    });
+
+    function clearIntro() {
+        const introDivs = document.querySelectorAll('.introcontent');
+        introDivs.forEach(function(node) {
+            node.classList.toggle('disappear');
+        });
+        introDivs[0].addEventListener('animationend', () => {
+            for (let i = 0; i < introDivs.length ; i++) {
+                introDivs[i].remove();
+            }
+            commentBox.classList.toggle('before');
+            commentBox.classList.add('noplay');
+            commentText.textContent = 'Oh.';
+        });
+
+    }
 
     function startPlay() {
         const introDivs = document.querySelectorAll('.introcontent');
@@ -63,41 +113,9 @@ window.onload = () => {
             weapons.forEach(function(node) {
                 node.classList.toggle('slidefrombelow');
             });
-            pointsText.setAttribute('class', 'grow');
-            
+            pointsText.setAttribute('class', 'grow');   
         });
-    }   
-
-    const commentText = document.createElement('p');
-    commentBox.appendChild(commentText);
-    pointsText.innerHTML = '0 - 0';
-    scoreBox.appendChild(pointsText);
-    const weaponsDivs = document.querySelectorAll('.weapons');
-
-    weaponsDivs.forEach(function(node) {
-        node.addEventListener('mouseover', weaponsHover);
-        node.addEventListener('mouseout', weaponsOff);
-    });
-    
-    weaponsDivs.forEach(function(node) {
-        node.addEventListener('click', function(e) {
-            if (e.target.id === 'rockimg' || e.target.id === 'rockbox') {
-                playerThisRound = 'Rock';
-            } else if (e.target.id === 'paperimg' || e.target.id === 'paperbox') {
-                playerThisRound = 'Paper';
-            } else if (e.target.id === 'scissorimg' || e.target.id === 'scissorbox') {
-                playerThisRound =  'Scissors';
-            }
-            playRound(playerThisRound, computerPlay());
-            pointsText.innerHTML = `${playerPoints} - ${computerPoints}`;
-            if (roundsPlayed === 5) {
-                results();
-            }
-        });
-    });
-
-    
-
+    } 
 
     function weaponsHover(e) {
         e.currentTarget.querySelector('img').style.transform = 'scale(1.06)';
@@ -169,9 +187,9 @@ window.onload = () => {
     }  
 
     function results() {
-        commentText.classList.toggle('fadeforresults');
+        commentText.classList.add('fadeforresults');
         commentText.addEventListener('animationend', () => {
-            commentText.classList.toggle('fadeforresults');
+            commentText.classList.remove('fadeforresults');
             if (playerPoints > computerPoints) {
                 commentText.textContent = 
                 `You've won! You got ${playerPoints} and the computer got ${computerPoints}.`;
@@ -183,24 +201,39 @@ window.onload = () => {
                 `It's a draw.`;       
             }
             weaponsDivs.forEach(function(node) {
-                node.classList.toggle('slidefrombelow');
-                node.classList.toggle('slidetobelow');
+                node.classList.remove('slidefrombelow');
+                node.classList.add('slidetobelow');
             });
-            weaponsDivs[2].addEventListener('animationend', () => {
-                let weaponIcons = document.getElementById('weaponicons');
-                weaponBox.insertBefore(replayButton, weaponIcons);
-            });
-            replayButton.addEventListener('click', () => {
-                replayButton.classList.toggle('disappear2');
-                weaponsDivs.forEach(function(node) {
-                    node.classList.toggle('slidefrombelow');
-                    node.classList.toggle('slidetobelow');
-                });
-                weaponsDivs[2].addEventListener('animationend', () => {
-                    commentText.textContent = '';
-                    pointsText.innerHTML = '0 - 0';
-                });
-            });
+            weaponsDivs[2].addEventListener('animationend', replayButtonToggle);
+            replayButton.addEventListener('click', reset);
         });
+    }
+
+    function reset() {
+        replayButton.classList.add('disappear2');
+        weaponsDivs.forEach(function(node) {
+            node.classList.add('slidefrombelow');
+            node.classList.remove('slidetobelow');
+        });
+        weaponsDivs[1].addEventListener('animationend', resetGameVar);
+        replayButton.removeEventListener('click', reset);
+    }
+
+    function replayButtonToggle() {
+        if (document.querySelector('button')) {
+            replayButton.classList.remove('disappear2');
+        } else {
+            weaponBox.insertBefore(replayButton, weaponIcons);
+        }
+        weaponsDivs[2].removeEventListener('animationend', replayButtonToggle);
+    }
+
+    function resetGameVar() {
+        commentText.textContent = '';
+        pointsText.innerHTML = '0 - 0';
+        computerPoints = 0;
+        playerPoints = 0;
+        roundsPlayed = 0;
+        weaponsDivs[1].removeEventListener('animationend', resetGameVar);
     }
 }
